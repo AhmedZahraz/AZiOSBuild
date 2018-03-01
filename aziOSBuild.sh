dbf36e28f1bc7d4bee1eb243914da0dbabce7dfb
Ahmed <ahmed.zahraz@hotmail.fr>
2018-03-01 11:58:35 +0100

    Add distributionMethod option fix bugs when creating exportOptions.plist

--- aziOSBuild.sh
+++ aziOSBuild.sh
@@ -10,11 +10,18 @@ PROVISIONING_PROFILE_PATH=""
 CONFIGURATION=""
 SCHEME=""
 SDK=""
+DISTRIBUTION_METHOD=""
+#UPLOAD_SYMBOLS=""
+#UPLOAD_BITCODE=""
 
 UUID=""
 PROJECT_EXTENSION=""
 CERT_NAME=""
 TEAM_ID="" 
+
+AUTO_CREATED_BUILD_FOLDER_NAME="AZ_AUTO_CREATED_BUILD_FOLDER"
+EXPORT_OPTION_PLIST_FILE="exportOptions.plist"
+ARCHIVE_FILE="autoCreatedArchive.xcarchive"
 
 function checkArgs()
 {
@@ -24,7 +31,7 @@ function checkArgs()
 		echo "***************************************************************************************"
     	read XCODE_PROJ_OR_XCWORKSPACE_PATH
     	echo "***************************************************************************************"
-    	echo "2- Please enter your Export path (the generated IPA will be created at this location):"
+    	echo "2- Please enter your Export path (specifies the destination of the generated IPA):"
     	echo "***************************************************************************************"
     	read EXPORT_PATH
     	echo "***************************************************************************************"
@@ -51,6 +58,10 @@ function checkArgs()
     	printf "8- Please enter the SDK name (iphoneos11.2, ..ect).\n  Notice that you can run xcodebuild -showsdks to see the list of installed sdk:\n"
     	echo "***************************************************************************************"
     	read SDK
+    	echo "***************************************************************************************"
+    	printf "9- Please enter the method of distribution.\n  Available options: app-store, ad-hoc, enterprise, development\n"
+    	echo "***************************************************************************************"
+    	read DISTRIBUTION_METHOD
 	elif [ "$#" -lt 7 ]; then
     	echo "Not enough arguments : you should pass XCODE_PROJ_OR_XCWORKSPACE_PATH EXPORT_PATH P12_PATH P12_PASSWORD PROVISIONING_PROFILE_PATH CONFIGURATION SCHEME SDK in this order (SDK is not required) - You can run xcodebuild -list in your project folder to see the list of configuration(s) and scheme(s) - You can run xcodebuild -showsdks to see the list of installed sdk"
     	exit
@@ -101,6 +112,11 @@ function checkArgs()
 		    shift # past argument
 		    shift # past value
 		    ;;
+		    -l|-distributionMethod)
+		    DISTRIBUTION_METHOD="$2"
+		    shift # past argument
+		    shift # past value
+		    ;;
 		esac
 		done
 		# XCODE_PROJ_OR_XCWORKSPACE_PATH=$1
@@ -113,28 +129,33 @@ function checkArgs()
 		# SDK=$8
 	fi
 
-	projectName=$(basename "$XCODE_PROJ_OR_XCWORKSPACE_PATH")
-	PROJECT_EXTENSION="${projectName##*.}"
-
   	if [ ! -e "${XCODE_PROJ_OR_XCWORKSPACE_PATH}" ]; then
     	echo "XCODE_PROJ_OR_XCWORKSPACE_PATH not found, verify your path "$XCODE_PROJ_OR_XCWORKSPACE_PATH
     	exit
-    elif [ "$PROJECT_EXTENSION" != "xcodeproj" ] && [ "$PROJECT_EXTENSION" != "xcworkspace" ]; then
-    	echo "XCODE_PROJ_OR_XCWORKSPACE_PATH should be with 'xcodeproj' or 'xcworkspace' extension"
+    fi
+	projectName=$(basename "$XCODE_PROJ_OR_XCWORKSPACE_PATH")
+	PROJECT_EXTENSION="${projectName##*.}"
+
+
+    if [ "$PROJECT_EXTENSION" != "xcodeproj" ] && [ "$PROJECT_EXTENSION" != "xcworkspace" ]; then
+    	echo "project should be with 'xcodeproj' or 'xcworkspace' extension"
     	exit
   	elif [ ! -e "${EXPORT_PATH}" ]; then
   		mkdir -p "$EXPORT_PATH"
   		if [ $? -eq 0 ]; then
     		echo "EXPORT PATH created : "$EXPORT_PATH
 		else
-    		echo "EXPORT_PATH is invalid : verify your path "$EXPORT_PATH
+    		echo "exportPath is invalid : verify your path "$EXPORT_PATH
     		exit
 		fi
    	elif [ ! -e "${P12_PATH}" ]; then
-    	echo "P12_PATH not found, verify your path "$P12_PATH
+    	echo "p12Path not found, verify your path "$P12_PATH
     	exit
    	elif [ ! -e "${PROVISIONING_PROFILE_PATH}" ]; then
-    	echo "PROVISIONING_PROFILE_PATH not found, verify your path "$PROVISIONING_PROFILE_PATH
+    	echo "provisioningProfilePath not found, verify your path "$PROVISIONING_PROFILE_PATH
+    	exit
+    elif [ "$DISTRIBUTION_METHOD" != "app-store" ] && [ "$DISTRIBUTION_METHOD" != "enterprise" ] && [ "$DISTRIBUTION_METHOD" != "ad-hoc" ] && [ "$DISTRIBUTION_METHOD" != "development" ]; then
+    	echo "distributionMethod should be app-store or ad-hoc or enterprise or development"
     	exit
   	else
      	echo "build start ..."
@@ -145,19 +166,22 @@ function checkArgs()
 function extractTeamID()
 {
 ####pyhton based solution
-CERT_NAME=$(python - "$P12_PATH" "$P12_PASSWORD" <<END 
-import sys
-from OpenSSL.crypto import *
-p12 = load_pkcs12(file(sys.argv[1], 'rb').read(), sys.argv[2])
-print(p12.get_friendlyname())
-END)
+# CERT_NAME=$(python - "${P12_PATH}" "${P12_PASSWORD}" <<END 
+# import sys
+# from OpenSSL.crypto import *
+# p12 = load_pkcs12(file(sys.argv[1], 'rb').read(), sys.argv[2])
+# print(p12.get_friendlyname())
+# END)
+# echo $CERT_NAME
 #####bash based solution
-#CERT_NAME=$(openssl pkcs12 -in "$P12_PATH" -nodes -passin pass:"$P12_PASSWORD" | openssl x509 -noout -subject | awk -F'[=/]' '{print $6}')
+CERT_NAME=$(openssl pkcs12 -in "${P12_PATH}" -nodes -passin pass:"${P12_PASSWORD}" | openssl x509 -noout -subject | awk -F'[=/]' '{print $6}')
 if [ -z "$CERT_NAME" ]; then
 	echo "Error 1 : cannot extract TeamID from p12 file : verify your password"
     exit
 fi
-TEAM_ID=$(echo $CERT_NAME | cut -d "(" -f2 | cut -d ")" -f1)
+TEAM_ID=$(openssl pkcs12 -in "${P12_PATH}" -nodes -passin pass:"${P12_PASSWORD}" | openssl x509 -noout -subject | awk -F'[=/]' '{print $4}')
+##Extract TEAM_ID from get_friendlyname
+#TEAM_ID=$(echo $CERT_NAME | cut -d "(" -f2 | cut -d ")" -f1)
 if [ -z "$TEAM_ID" ]; then
 	echo "Error 2 : cannot extract TeamID from p12 file"
     exit
@@ -171,12 +195,26 @@ function extractUUID() {
 		echo "Error 3 : PROVISIONING_PROFILE_PATH not found in path "$PROVISIONING_PROFILE_PATH
     	exit
 	fi
-	UUID=$(grep "<key>UUID</key>" "$PROVISIONING_PROFILE_PATH" -A 1 --binary-files=text | sed -E -e "/<key>/ d" -e "s/(^.*<string>)//" -e "s/(<.*)//")
+	UUID=$(grep "<key>UUID</key>" "${PROVISIONING_PROFILE_PATH}" -A 1 --binary-files=text | sed -E -e "/<key>/ d" -e "s/(^.*<string>)//" -e "s/(<.*)//")
 	if [ -z "$UUID" ]; then
 	echo "Error 4 : cannot extract UUID from your PROVISIONING_PROFILE_PATH."
     exit
 	fi
 	echo "UUID = "$UUID
+}
+
+#Extract App ID from 
+function extractAppID() {
+	if [ -z "$PROVISIONING_PROFILE_PATH" ]; then
+		echo "Error 3bis : PROVISIONING_PROFILE_PATH not found in path "$PROVISIONING_PROFILE_PATH
+    	exit
+	fi
+	APP_ID=$(eval "security cms -D -i \""${PROVISIONING_PROFILE_PATH}"\" | sed -n 's/.*<string>"${TEAM_ID}".\([^<*]*\)<\/string>.*/\1/p'")
+	if [ -z "$APP_ID" ]; then
+	echo "Error 4bis : cannot extract APP_ID from your PROVISIONING_PROFILE_PATH."
+    exit
+	fi
+	echo "APP_ID = "$APP_ID
 }
 
 # Import p12 to system keychain
@@ -196,6 +234,7 @@ function importP12ToSystemKeychain() {
 
 # Install provisioning Profile <==> copy it to $HOME/Library/MobileDevice/Provisioning Profiles/
 function installProvisioningProfile() {
+	#to read PP ==> security cms -D -i ...mobileprovision
 	if [ -z "$PROVISIONING_PROFILE_PATH" ]; then
 		echo "Error 9 : PROVISIONING_PROFILE_PATH not found in path "$PROVISIONING_PROFILE_PATH
     	exit
@@ -219,27 +258,38 @@ function createExportOptionsPlistFile() {
 		echo "Error 8 : teamID is empty"
     	exit
 	fi
+	if [ "$DISTRIBUTION_METHOD" != "app-store" ] && [ "$DISTRIBUTION_METHOD" != "enterprise" ] && [ "$DISTRIBUTION_METHOD" != "ad-hoc" ] && [ "$DISTRIBUTION_METHOD" != "development" ]; then
+    	echo "DISTRIBUTION_METHOD should be app-store or ad-hoc or enterprise or development"
+    	exit
+	fi
 	exportOptions='<?xml version="1.0" encoding="UTF-8"?>
 	<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 	<plist version="1.0">
 	<dict>
 	    <key>provisioningProfiles</key>
 	    <dict>
-	        <key>com.trigonetech.ayruu</key>
+	        <key>'$APP_ID'</key>
 	        <string>'$UUID'</string>
 	    </dict>
 	    <key>method</key>
-	    <string>app-store</string>
+	    <string>'$DISTRIBUTION_METHOD'</string>
 	    <key>teamID</key>
 	    <string>'$TEAM_ID'</string>
 	</dict>
 	</plist>'
-	echo $exportOptions >> exportOptions.plist
+	mkdir -p "$AUTO_CREATED_BUILD_FOLDER_NAME"
+	if [ $? -eq 0 ]; then
+		echo $AUTO_CREATED_BUILD_FOLDER_NAME" folder created!"
+	else
+		echo "Error 99 : cannot create auto build folder"
+		exit
+	fi
+	echo $exportOptions > "$AUTO_CREATED_BUILD_FOLDER_NAME/$EXPORT_OPTION_PLIST_FILE"
 	#tee exportOptions.plist <<<$exportOptions
 	if [ $? -eq 0 ]; then
-    	echo "File exportOptions.plist created."
+    	echo "File $EXPORT_OPTION_PLIST_FILE created."
 	else
-    	echo "Error 10 : cannot create exportOptions.plist"
+    	echo "Error 10 : cannot create $EXPORT_OPTION_PLIST_FILE"
     	exit
 	fi
 }
@@ -256,7 +306,7 @@ function createArchive() {
 	if [ -z "$SDK" ]; then
 		SDK="iphoneos"
 	fi
-	ARCHIVE_PATH="./autoCreatedArchive.xcarchive"
+	ARCHIVE_PATH="$AUTO_CREATED_BUILD_FOLDER_NAME/$ARCHIVE_FILE"
 	#xcodebuild -project TestResignApp.xcodeproj -target TestResignApp -sdk iphoneos11.2 -configuration Release -archivePath /Users/ahmed/Documents/ProjectRetD/TestResignApp/myArchive/ahmedArchiveNew.xcarchive  -scheme TestResignApp PROVISIONING_PROFILE_SPECIFIER="AyruuProvisioningProfilesDistribution" CODE_SIGN_IDENTITY="iPhone Distribution: Trigone Tech (HLHA86H3UL)" archive
 	if [ "$PROJECT_EXTENSION" == "xcodeproj" ]; then
 		PROJECT_INPUT_TYPE="project"
@@ -277,7 +327,7 @@ function createArchive() {
 
 function createIPA() {
 	#xcodebuild -exportArchive -archivePath /Users/ahmed/Documents/ProjectRetD/TestResignApp/myArchive/ahmedArchiveNew.xcarchive -exportOptionsPlist exportOptions.plist -exportPath $PWD/build
-	xcodebuild -exportArchive -archivePath "${ARCHIVE_PATH}" -exportOptionsPlist exportOptions.plist -exportPath "${EXPORT_PATH}"
+	xcodebuild -exportArchive -archivePath "${ARCHIVE_PATH}" -exportOptionsPlist "$AUTO_CREATED_BUILD_FOLDER_NAME/$EXPORT_OPTION_PLIST_FILE" -exportPath "${EXPORT_PATH}"
 	if [ $? -eq 0 ]; then
     	echo "IPA file created at "$EXPORT_PATH
 	else
@@ -289,6 +339,7 @@ function createIPA() {
 checkArgs "$@"
 extractTeamID
 extractUUID
+extractAppID
 importP12ToSystemKeychain
 installProvisioningProfile
 createExportOptionsPlistFile
